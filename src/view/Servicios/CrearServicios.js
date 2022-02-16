948b0f9758e75a0f8728ff3f9c10adcf5075655e
tsubakijuju13 <santiago.ramirez.ospina@correounivalle.edu.co>
2022-02-16 16:20:06 -0500

    CreateOS

--- src/view/Servicios/CrearServicios.js
+++ src/view/Servicios/CrearServicios.js
@@ -1,8 +1,22 @@
 import BadgeServicio from "./FooterServicios/BadgeServicio"
 import { FormControl, Button, Form, Row, Col } from "react-bootstrap";
 import Navbar from "../componentes/Navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const CrearServicios = () => {
+
+    const url = "http://localhost:5000/IDclientes";
+    const[data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setData(response.data)
+        });
+    }, []);
+
+    if (!data) return(<div>Error</div>);
+
     const handleAddFormChange = () => {
 
     };
@@ -18,25 +32,50 @@ export const CrearServicios = () => {
             <Form onSubmit={handleAddFormSubmit} style={{width: "50%"}}>
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formBasicEmail">
-                        <Form.Label>Servicio</Form.Label>
-                        <Form.Control name="fullName" type="text" placeholder="Servicio" onChange={handleAddFormChange}/>
+                        <Form.Label>Asignar Empleado</Form.Label>
+                        <Form.Select aria-label="Default select example">
+                            <option>Funcionario</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formBasicPassword">
-                        <Form.Label>Valor del Servicio</Form.Label>
-                        <Form.Control name="address" type="text" placeholder="Valor del Servicio" onChange={handleAddFormChange}/>
+                        <Form.Label>Estado</Form.Label>
+                        <Form.Select aria-label="Default select example">
+                            <option>Select Estado</option>
+                            <option value="1">Asignado</option>
+                            <option value="2">En tramite</option>
+                        </Form.Select>
                     </Form.Group>
                 </Row>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Funcionario Asignado</Form.Label>
-                    <Form.Control name="phoneNumber" type="text" placeholder="Funcionario" onChange={handleAddFormChange}/>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Comentarios</Form.Label>
+                    <Form.Control name="phoneNumber" as="textarea"  onChange={handleAddFormChange}/>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Estado</Form.Label>
-                    <Form.Control name="email" type="text" placeholder="Estado" onChange={handleAddFormChange}/>
-                </Form.Group>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formBasicPassword">
+                        <Form.Label>Costo Total</Form.Label>
+                        <Form.Control name="email" type="text" placeholder="$0000,00" onChange={handleAddFormChange}/>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formBasicPassword">
+                        <Form.Label>Cliente</Form.Label>
+                        <Form.Select aria-label="Default select example">
+                            <option>Select ID</option>
+                            {data.map((data_id, index) => {
+                                return(
+                                    <option value={index} key={index}>
+                                        {data_id.id}
+                                    </option>
+                                )
+                            })}
+                        </Form.Select>
+                    </Form.Group>
+                </Row>
 
                 <Button variant="primary" type="submit">
                     Submit
