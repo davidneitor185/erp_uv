a7548011990395f380ab39620e02ec41ba24e364
Laura <laura.valentina.quintero@correounivalle.edu.co>
2022-02-26 21:18:07 -0500

    perfile

--- src/view/roles_y_perfiles/RolesPerfiles.js
+++ src/view/roles_y_perfiles/RolesPerfiles.js
@@ -1,71 +1,125 @@
-import React, { useState }  from 'react'
+import React, { useState } from "react";
 import Navbar from "../componentes/Navbar";
 import { FormControl, Button, Form, Row, Col, Badge } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { TablaSI } from "../componentes/TablaSI";
+import {
+  TablePagination,
+  TableRow,
+  TableCell,
+} from "../componentes/TablaPagination";
 import useAxios from "../../useAxios";
-import {BsFillPersonPlusFill} from 'react-icons/bs'
+import { BsFillPersonPlusFill } from "react-icons/bs";
+import { FaUserTimes, FaUserEdit } from "react-icons/fa";
+import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 const RolesPerfiles = () => {
-    const titulos = [
-        "Identificación",
-        "Nombre",
-        "Teléfono",
-        "Email",
-        "Rol",
-        "Opciones",
-      ];
-      const [datos, setDatos] = useState([]);
-      const {data} = useAxios("/solicitudes");
-      const tipo = "solicitudes";
+  const titulos = [
+    "Identificación",
+    "Nombre",
+    "Teléfono",
+    "Email",
+    "Rol",
+    "Opciones",
+  ];
+  /**/
+  const { data: usuarios } = useAxios("/usuarios");
+
   return (
     <>
-      <Navbar/>
+      <Navbar />
 
       <div style={{ display: "flex", justifyContent: "center", marginTop: 40 }}>
-        <h3>Roles & Perfiles</h3>
+        <h3> Perfiles</h3>
       </div>
       <div>
-          <Form>
-              <Form.Group as={Row} style={{ justifyContent: "center" }}>
-              <Col sm="3">
+        <Form>
+          <Form.Group as={Row} style={{ justifyContent: "center" }}>
+            <Col sm="3">
               <FormControl
                 placeholder="   Search..."
                 aria-label="Username"
                 aria-describedby="basic-addon1"
               />
             </Col>
-            <Col sm="1" style={{ width: "4%", display: "flex"}}>
+            <Col sm="1" style={{ width: "4%", display: "flex" }}>
               <Button variant="secondary">🔍</Button>
             </Col>
-            <Col sm="2" style={{ width: "5%", display: "flex"}}>
-              <Link className="btn btn-secondary" to={"/rolesperfiles/crear"}><BsFillPersonPlusFill style={{fontSize:25}}/></Link>
+            <Col sm="2" style={{ width: "5%", display: "flex" }}>
+              <Link className="btn btn-secondary" to={`/rolesperfiles/crear`}>
+                <BsFillPersonPlusFill style={{ fontSize: 25 }} />
+              </Link>
             </Col>
-              </Form.Group>
-          </Form>
+          </Form.Group>
+        </Form>
       </div>
       <div style={{ justifyContent: "center", margin: "0 250px" }}>
-        <TablaSI titulos={titulos} datos={datos} tipo={tipo}/>
+        <TablePagination
+          titulos={titulos}
+          /*CUANTOS POR PAG*/
+          rowsPerPage={5}
+        >
+          {usuarios &&
+            usuarios.map((usuario) => {
+              return (
+                <TableRow>
+                  <TableCell key={1}>{usuario.identificacion}</TableCell>
+                  <TableCell key={2}>{usuario.nombre_funcionario}</TableCell>
+                  <TableCell key={3}>{usuario.tel}</TableCell>
+                  <TableCell key={4}>{usuario.email}</TableCell>
+                  <TableCell key={5}>{usuario.nombre_rol}</TableCell>
+                  <TableCell>
+                    <OverlayTrigger
+                      delay={{ hide: 450, show: 180 }}
+                      overlay={(props) => (
+                        <Tooltip {...props}>Eliminar</Tooltip>
+                      )}
+                      placement="bottom"
+                    >
+                      <FaUserTimes
+                        style={{
+                          fontSize: 25,
+                          color: "black",
+                          marginRight: 10,
+                        }}
+                      />
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                    delay={{ hide: 450, show: 180 }}
+                    overlay={(props) => (
+                      <Tooltip {...props}>Modificar</Tooltip>
+                    )}
+                    placement="bottom"
+                    >
+                    <Link to={`/rolesperfiles/modificar/${usuario.id_funcionario}`}>
+                      <FaUserEdit style={{ fontSize: 25, color: "black" }} />
+                    </Link>
+                    </OverlayTrigger>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+        </TablePagination>
       </div>
       <footer>
-  <div style={{paddingLeft:25}}>
-      <div>
-  <Badge pill bg="primary" >
-    <Link to={''} className='link'>
-  1. Perfiles
-  </Link>
-  </Badge>{' '}
-  <Badge pill bg="secondary">
-  <Link to={''} className='link'>
-    2. Roles
-  </Link>
-  </Badge>{' '}
-</div>
-  </div>;
-  </footer>
-        
-      </>
-  )
-}
+        <div style={{ paddingLeft: 25 }}>
+          <div>
+            <Badge pill bg="primary">
+              <Link to={""} className="link">
+                1. Perfiles
+              </Link>
+            </Badge>{" "}
+            <Badge pill bg="secondary">
+              <Link to={"/rolesperfiles/rol"} className="link">
+                2. Roles
+              </Link>
+            </Badge>{" "}
+          </div>
+        </div>
+        ;
+      </footer>
+    </>
+  );
+};
 
-export default RolesPerfiles
+export default RolesPerfiles;
